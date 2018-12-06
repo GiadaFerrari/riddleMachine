1059a89f88a55ed04de66634ab03c64c9b9c4ba9
Giada Ferrari <giadaferrari@Giadas-MacBook-Pro.local>
2018-12-06 13:12:48 -0500

    defined players

--- script.js
+++ script.js
@@ -3,7 +3,19 @@ let p1 = document.querySelector('.p1');
 let p2 = document.querySelector('.p2');
 let answers;
 let speechRec = new p5.SpeechRec('en-US', gotSpeech);
+let aside1 = document.querySelector('.p1pt');
+let aside2 = document.querySelector('.p2pt');
+let p1pt = 0;
+let p2pt = 0;
+let tryAgain = document.querySelector('.try');
+let good = document.querySelector('.good');
+let player;
 
+
+
+
+aside1.textContent = p1pt;
+aside2.textContent = p2pt;
 
 
 
@@ -16,17 +28,37 @@ function fetchRiddle() {
 }
 
 function setup() {
-    noCanvas();
     let speechRec = new p5.SpeechRec('en-US', gotSpeech);
 }
 
 function gotSpeech() {
-    console.log(speechRec)
+
+    console.log(speechRec.resultString);
+    if (speechRec.resultString == answers) {
+        console.log(good)
+        //good.addClass.toggle('hide');
+        if (player == 'player1') {
+            p1pt++;
+
+        };
+        if (player == 'player2') {
+            p2pt++
+        }
+        fetchRiddle();
+    }
+    //     else {
+    //        tryAgain.addClass.toggle('hide');
+    //    }
+    //    setTimeout(() => {
+    //        if (tryAgain.hasClass('hide') != true) {
+    //            tryAgain.addClass.toggle('hide')
+    //        } else if (good.hasClass('hide') != true) {
+    //            good.addClass.toggle('hide')
+    //        }
+    //
+    //    }, 2000);
 }
 
-function draw() {
-
-}
 
 
 function show(riddles) {
@@ -38,14 +70,16 @@ function show(riddles) {
     let index = myIndex(riddles.length)
     p.textContent = riddles[index].riddle;
     answers = riddles[index].answer;
-    console.log(riddles.length)
+    console.log(answers)
 }
 
 fetchRiddle();
 
-function answer(player) {
-    console.log(player + ' replied');
+function answer(playerA) {
+    console.log('im listening')
+    console.log(playerA + ' replied');
     speechRec.start();
+    player == playerA;
 
 
 }
